ac7d7c1ae51aec1f067cb3f88ca40a7424dafc9a
PhyoPhyoMa <phyophyoma18@gmail.com>
2018-08-26 01:52:56 +0630

    Main Page

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, url_for, request, jsonify
+import zg2uni
+import uni2zg
+import win2uni
+import uni2win
 
 myapp=Flask(__name__)
 
@@ -10,10 +14,24 @@ def hello():
 def convert():
     myinput = request.form['myinput']
     myoutput = request.form['myoutput']
+    source = request.form['source']
     output = "The Input is " + myinput +"\r\n" + "The Output is " +myoutput
-    if myinput:
-        return jsonify({'output': output})
-    return jsonify({'output': "An error occured"})
+    if myinput == "zawgyi" and myoutput == "unicode":
+        output = zg2uni.convert(source)
+    if myinput == "zawgyi" and myoutput == "winmyanmar":
+        output = zg2uni.convert(uni2win.convert(source))
+    if myinput == "unicode" and myoutput == "zawgyi":
+        output = uni2zg.convert(source)
+    if myinput == "unicode" and myoutput == "winmyanmar":
+        output = uni2win.convert(source)
+    if myinput == "winmyanmar" and myoutput == "zawgyi":
+        output = uni2zg.convert(win2uni.convert(source))
+    if myinput == "winmyanmar" and myoutput == "unicode":
+        output = win2uni.convert(source)
+    if myinput == myoutput:
+        output = "Input is: " + myinput + "and Output is: " + \
+                myoutput + ". There are both same.No conversion done!"
+    return jsonify ({'output': output})
 
 if __name__ == "__main__":
     myapp.run(debug=True)
