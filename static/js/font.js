b2e22637b68032a2027476eef4b6e2eb6d6dbf65
PhyoPhyoMa <phyophyoma18@gmail.com>
2018-08-26 09:28:27 +0630

    font.js

---
+++ static/js/font.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+$('input[type=radio][name=input]').change(function(){
+		if (this.value == 'zawgyi') {
+			consloe.log("Zawgyi selected");
+		}
+		else if (this.value == 'unicode') {
+			console.log("Uicode selected");
+		}
+	});
+
+$('input[type=radio][name=output]').change(function(){
+	if (this.value == 'zawgyi') {
+		console.log("Zawgyi selected");
+	}
+	else if (this.value == 'unicode') {
+		console.log("Unicode selected");
+	}
+});
+});
